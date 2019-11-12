28d004ff90958d475991e9e9600dad9ecaa9c3af
dqw <761945125@qq.com>
2019-11-12 15:50:45 +0800

    注释

--- src/main/java/com/dqw/thrift/client/ThriftClient.java
+++ src/main/java/com/dqw/thrift/client/ThriftClient.java
@@ -16,20 +16,26 @@ import java.lang.annotation.*;
 @Documented
 public @interface ThriftClient {
     /**
-     * 实例名称
+     * 实例名称,可能会存在多个实例,例如订单/用户/会员,这里表示选择使用哪个实例
      *
      * @return String
      */
     String instances();
 
     /**
-     * 服务实例注册的服务名称(服务提供者的名称),默认为类字母小写
+     * 每个实例里面会存在多个ThriftProvider,这里指定使用哪一个ThriftProvider
      * <p>
      * 例如服务提供者:
      * <@ThriftProvider>
      * public class HelloAgentImpl implements HelloAgent.Iface {
      * }
      * 此服务的名称为:helloAgentImpl
+     * <p>
+     * 例如服务提供者:
+     * <@ThriftProvider(serviceName="helloAgent")>
+     * public class HelloAgentImpl implements HelloAgent.Iface {
+     * }
+     * 此服务的名称为:helloAgent
      * </p>
      *
      * @return String
